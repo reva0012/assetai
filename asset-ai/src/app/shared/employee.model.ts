0b482d55bce94b8b2db224e4e5a21e379bfb94dd
reva munish <63241521+reva0012@users.noreply.github.com>
2021-10-20 14:48:09 +0530

    Add files via upload

---
+++ asset-ai/src/app/shared/employee.model.ts
@@ -0,0 +1,32 @@
+export class Employee {
+    _id: string;
+    Company: string;
+    Fleet: string;
+    VesselIMO: string;
+    VesselName: string;
+    Equipment : string;
+    Parameter: string;
+    Count: string;
+    TotalDuration: string;
+    LongestDuration: string;
+    LongestDurationStartTime: string;
+    AlertType: string;
+    Status: string;
+    AOTRemarks: string;
+    MarkAlert: string;
+    RootCause: string;
+    ActionTaken : string;
+    UseCase : string;
+    CostAvoidance: string;
+    RemarksforCostAvoidance: string;
+    RoleofSMARTShip: string;
+    PotentialSeverity: string;
+    PotentialConsequence: string;
+    Screenshot: string;
+    Month: string;
+    CurrentTSI: string;
+    EmailID: string;
+}
+
+
+
